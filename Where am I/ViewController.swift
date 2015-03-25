35ddcda3765a85fc4ff459c6e484a253aa8b1c36
Joshua Raichur <hello@joshuaraichur.com>
2015-03-25 16:38:43 +0530

    layout and corelocation code

--- Where am I/ViewController.swift
+++ Where am I/ViewController.swift
@@ -9,13 +9,48 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var heading: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
+    var manager : CLLocationManager!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
     }
 
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userLocation : CLLocation = locations[0] as CLLocation
+        
+        latitude.text = "\(userLocation.coordinate.latitude)"
+        longitude.text = "\(userLocation.coordinate.longitude)"
+        heading.text = "\(userLocation.course)"
+        speed.text = "\(userLocation.speed)"
+        altitude.text = "\(userLocation.altitude)"
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: {(placemarks, error) in
+        
+            if error != nil { println(error) }
+            else {
+                let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                self.address.text = "\(p.subThoroughfare) \(p.thoroughfare)"
+                
+            }
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
